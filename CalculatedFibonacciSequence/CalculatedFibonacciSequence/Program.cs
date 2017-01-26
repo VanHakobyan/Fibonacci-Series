ca8fbe092180138e448f2250795429a2b245136e
VanHakobyan <van19962013@mail.ru>
2017-01-26 21:43:18 +0400

    exemple

---
+++ CalculatedFibonacciSequence/CalculatedFibonacciSequence/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatedFibonacciSequence
+{
+    class Program
+    {
+        static IEnumerable<int> GetCalculatedFibonacciSequence()
+        {
+            var current = 1;
+            var b = 0;
+            var next = 0;
+            yield return next;
+            yield return current;
+            while (true)
+            {
+                next = current + b;
+                yield return next;
+                b = current;
+                current = next;
+            }
+        }
+        static void Main(string[] args)
+        {
+            foreach (var item in GetCalculatedFibonacciSequence().Skip(0).Take(100))
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
